ff9bdc8e4ffb2ba5917d6fc3f57b1063cc669548
SHRESHTA ANUGU <131268085+shre-45@users.noreply.github.com>
2025-07-29 14:38:50 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,6 +2,7 @@ def call(String Project, String ImageTag, String acrName) {
     withCredentials([usernamePassword(credentialsId: 'acr-credentials', usernameVariable: 'acrUser', passwordVariable: 'acrPass')]) {
         sh """
         echo "${acrPass}" | docker login ${acrName}.azurecr.io -u "${acrUser}" --password-stdin
+        docker tag ${Project}:${ImageTag} ${acrName}.azurecr.io/${Project}:${ImageTag}
         docker push ${acrName}.azurecr.io/${Project}:${ImageTag}
         """
     }
