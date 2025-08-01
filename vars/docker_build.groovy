547f59fc1bdab166198a3342c700d534c99b62d0
SHRESHTA ANUGU <131268085+shre-45@users.noreply.github.com>
2025-08-01 18:19:50 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,4 @@
-def call(String app, String tag) {
-    dir(path) {
-        sh "docker build --no-cache -t ${app}:${tag} ."
-    }
+def call(String ProjectName, String ImageTag){
+  sh "docker build -t ${ProjectName}:${ImageTag} ."
+
 }
